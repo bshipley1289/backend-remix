ecb49268381903798f36f740bdc3858c3c3d681e
Brandon Shipley <bshipley1289@gmail.com>
2021-04-25 18:23:14 -0500

    fine tuning database

--- app.js
+++ app.js
@@ -7,89 +7,87 @@ const port = 4000;
 app.use(express.json());
 
 // Database
-let db = {
-  users: [
-    {
-      id: nanoid(),
-      firstName: "Brandon",
-      lastName: "Shipley",
-      username: "bshipley",
-      password: "12345",
-      email: "bshipley1289@gmail.com",
-      phoneNumber: "903-721-1203",
-      createdAt: Date.now(),
-      todos: [
-        {
-          id: nanoid(),
-          title: "ScrumMaster",
-          details: "Doing the scrum stuff",
-          completed: false,
-          priority: "High",
-          tag: "default",
-        },
-      ],
-    },
-    {
-      id: nanoid(),
-      firstName: "Terry",
-      lastName: "Cross",
-      username: "TCross",
-      password: "12345",
-      email: "tcrossband.86@gmail.com",
-      phoneNumber: "909-587-8922",
-      createdAt: Date.now(),
-      todos: [
-        {
-          id: nanoid(),
-          title: "Project Owner",
-          details: "Boss people around!",
-          completed: false,
-          priority: "High",
-          tag: "default",
-        },
-      ],
-    },
-    {
-      id: nanoid(),
-      firstName: "Jared",
-      lastName: "Fajman",
-      username: "JFajman",
-      password: "12345",
-      email: "jmfajman@gmail.com",
-      phoneNumber: "520-318-4293",
-      createdAt: Date.now(),
-      todos: [
-        {
-          id: nanoid(),
-          title: "QA",
-          details: "Reviewing Code",
-          completed: false,
-          priority: "High",
-          tag: "default",
-        },
-      ],
-    },
-    {
-      id: nanoid(),
-      firstName: "Dawit",
-      lastName: "Lemma",
-      username: "DLemma",
-      email: "delfan2003@gmail.com",
-      phoneNumber: "000-000-0000",
-      createdAt: Date.now(),
-      todos: [
-        {
-          id: nanoid(),
-          title: "Backend",
-          details: "Setting up backend code",
-          completed: true,
-          priority: "High",
-          tag: "default",
-        },
-      ],
-    },
-  ],
-};
+let users = [
+  {
+    id: nanoid(),
+    firstName: "Brandon",
+    lastName: "Shipley",
+    username: "bshipley",
+    password: "12345",
+    email: "bshipley1289@gmail.com",
+    phoneNumber: "903-721-1203",
+    createdAt: Date.now(),
+    todos: [
+      {
+        id: nanoid(),
+        title: "ScrumMaster",
+        details: "Doing the scrum stuff",
+        completed: false,
+        priority: "High",
+        tag: "default",
+      },
+    ],
+  },
+  {
+    id: nanoid(),
+    firstName: "Terry",
+    lastName: "Cross",
+    username: "TCross",
+    password: "12345",
+    email: "tcrossband.86@gmail.com",
+    phoneNumber: "909-587-8922",
+    createdAt: Date.now(),
+    todos: [
+      {
+        id: nanoid(),
+        title: "Project Owner",
+        details: "Boss people around!",
+        completed: false,
+        priority: "High",
+        tag: "default",
+      },
+    ],
+  },
+  {
+    id: nanoid(),
+    firstName: "Jared",
+    lastName: "Fajman",
+    username: "JFajman",
+    password: "12345",
+    email: "jmfajman@gmail.com",
+    phoneNumber: "520-318-4293",
+    createdAt: Date.now(),
+    todos: [
+      {
+        id: nanoid(),
+        title: "QA",
+        details: "Reviewing Code",
+        completed: false,
+        priority: "High",
+        tag: "default",
+      },
+    ],
+  },
+  {
+    id: nanoid(),
+    firstName: "Dawit",
+    lastName: "Lemma",
+    username: "DLemma",
+    email: "delfan2003@gmail.com",
+    phoneNumber: "000-000-0000",
+    createdAt: Date.now(),
+    todos: [
+      {
+        id: nanoid(),
+        title: "Backend",
+        details: "Setting up backend code",
+        completed: true,
+        priority: "High",
+        tag: "default",
+      },
+    ],
+  },
+];
 
 // use
 
@@ -114,7 +112,7 @@ app.get("/", (req, res) => {
 
 // get all users
 app.get("/users", (req, res) => {
-  res.send(db.users);
+  res.send(users);
 });
 
 // login
@@ -122,16 +120,16 @@ app.post("/login/user", (req, res) => {
   if (!req.body.username || !req.body.password) {
     res.status(400).send("Missing required field");
   }
-  let selectedUser = db.users.findIndex(
+  let selectedUser = users.findIndex(
     (user) => user.username === req.body.username
   );
   if (selectedUser === -1) {
     res.status(404).send("Incorrect username or password");
   }
-  if (db.users[selectedUser].password !== req.body.password) {
+  if (users[selectedUser].password !== req.body.password) {
     res.status(404).send("Incorrect username or password");
   }
-  res.send(db.users[selectedUser]);
+  res.send(users[selectedUser]);
 });
 
 // logout
@@ -141,11 +139,11 @@ app.get("/logout", (req, res) => {
 
 // get single user by id
 app.get("/user/:userId", (req, res) => {
-  let selectedUser = db.users.findIndex((user) => user.id == req.params.userId);
+  let selectedUser = users.findIndex((user) => user.id == req.params.userId);
   if (selectedUser === -1) {
     res.status(404).send("User Not Found");
   }
-  res.send(db.users[selectedUser]);
+  res.send(users[selectedUser]);
 });
 
 // create user
@@ -164,71 +162,71 @@ app.post("/user/create", (req, res) => {
     createdAt: Date.now(),
     todos: [],
   };
-  db.users.push(newUser);
+  users.push(newUser);
   res.send("Successfully Created New User");
 });
 
 // edit user
 app.patch("/user/:userId", (req, res) => {
-  let selectedUser = db.users.findIndex((user) => user.id == req.params.userId);
+  let selectedUser = users.findIndex((user) => user.id == req.params.userId);
   if (selectedUser === -1) {
     res.status(404).send("User Not Found");
   }
   if (req.body.username) {
-    let checkingUsername = db.users.find(
+    let checkingUsername = users.find(
       (user) => user.username === req.body.username
     );
     if (checkingUsername.id !== req.params.userId) {
       res.status(403).send("Username not available");
     }
   }
-  db.users[selectedUser] = {
-    ...db.users[selectedUser],
-    firstName: req.body.firstName || db.users[selectedUser].firstName,
-    lastName: req.body.lastName || db.users[selectedUser].lastName,
-    username: req.body.username || db.users[selectedUser].username,
-    email: req.body.email || db.users[selectedUser].email,
-    phoneNumber: req.body.phoneNumber || db.users[selectedUser].phoneNumber,
+  users[selectedUser] = {
+    ...users[selectedUser],
+    firstName: req.body.firstName || users[selectedUser].firstName,
+    lastName: req.body.lastName || users[selectedUser].lastName,
+    username: req.body.username || users[selectedUser].username,
+    email: req.body.email || users[selectedUser].email,
+    phoneNumber: req.body.phoneNumber || users[selectedUser].phoneNumber,
   };
   res.send({
     status: "User edited successfully",
-    user: db.users[selectedUser],
+    user: users[selectedUser],
   });
 });
 
 // delete user
 app.delete("/user/:userId", (req, res) => {
-  let selectedUser = db.users.findIndex((user) => user.id == req.params.userId);
+  let selectedUser = users.findIndex((user) => user.id == req.params.userId);
   if (selectedUser === -1) {
     res.status(404).send("User Not Found");
   }
-  db = db.users.splice(selectedUser, 1);
+  db = users.splice(selectedUser, 1);
   res.send("Deleted Successfully");
 });
 
 // get todos list of user
 app.get("/user/:userId/todos", (req, res) => {
-  let selectedUser = db.users.findIndex((user) => user.id == req.params.userId);
+  let selectedUser = users.findIndex((user) => user.id == req.params.userId);
   if (selectedUser === -1) {
     res.status(404).send("User Not Found");
   }
-  res.send(db.users[selectedUser].todos);
+  res.send(users[selectedUser].todos);
 });
 
 // get single todo from user
 app.get("/user/:userId/todos/:todoId", (req, res) => {
-  let selectedUser = db.users.findIndex((user) => user.id == req.params.userId);
+  let selectedUser = users.findIndex((user) => user.id == req.params.userId);
   if (selectedUser === -1) {
     res.status(404).send("User Not Found");
   }
 
-  let selectedTodo = db.users[selectedUser].todos.findIndex(
+  let selectedTodo = users[selectedUser].todos.findIndex(
     (todo) => todo.id == req.params.todoId
   );
   if (selectedTodo === -1) {
     res.status(404).send("Todo not found");
   }
-  res.send(db.users[selectedUser].todos[selectedTodo]);
+  res.send(users[selectedUser].todos[selectedTodo]);
 });
 
 // add todo item
@@ -236,7 +234,7 @@ app.post("/user/:userId/todos/create", (req, res) => {
   if (!req.body.title) {
     res.status(400).send("Missing required field");
   }
-  let selectedUser = db.users.findIndex((user) => user.id == req.params.userId);
+  let selectedUser = users.findIndex((user) => user.id == req.params.userId);
   if (selectedUser === -1) {
     res.status(404).send("User Not Found");
   }
@@ -248,7 +246,7 @@ app.post("/user/:userId/todos/create", (req, res) => {
     priority: req.body.priority || "low",
     tag: req.body.tag || "default",
   };
-  db.users[selectedUser].todos.push(newTodo);
+  users[selectedUser].todos.push(newTodo);
   res.send("Todo Successfully Created");
 });
 
@@ -257,18 +255,18 @@ app.patch("/user/:userId/todos/:todoId", (req, res) => {
   if (!req.body.title) {
     res.status(400).send("Missing required field");
   }
-  let selectedUser = db.users.findIndex((user) => user.id == req.params.userId);
+  let selectedUser = users.findIndex((user) => user.id == req.params.userId);
   if (selectedUser === -1) {
     res.status(404).send("User Not Found");
   }
-  let selectedTodo = db.users[selectedUser].todos.findIndex(
+  let selectedTodo = users[selectedUser].todos.findIndex(
     (todo) => todo.id == req.params.todoId
   );
   if (selectedTodo === -1) {
     res.status(404).send("Todo not found");
   }
-  db.users[selectedUser].todos[selectedTodo] = {
-    ...db.users[selectedUser].todos[selectedTodo],
+  users[selectedUser].todos[selectedTodo] = {
+    ...users[selectedUser].todos[selectedTodo],
     title: req.body.title,
     details: req.body.details || "",
     completed: req.body.completed || false,
@@ -280,17 +278,17 @@ app.patch("/user/:userId/todos/:todoId", (req, res) => {
 
 // delete todo item
 app.delete("/user/:userId/todos/:todoId", (req, res) => {
-  let selectedUser = db.users.findIndex((user) => user.id == req.params.userId);
+  let selectedUser = users.findIndex((user) => user.id == req.params.userId);
   if (selectedUser === -1) {
     res.status(404).send("User Not Found");
   }
-  let selectedTodo = db.users[selectedUser].todos.findIndex(
+  let selectedTodo = users[selectedUser].todos.findIndex(
     (todo) => todo.id == req.params.todoId
   );
   if (selectedTodo === -1) {
     res.status(404).send("Todo not found");
   }
-  db = db.users[selectedUser].todos.splice(selectedTodo, 1);
+  db = users[selectedUser].todos.splice(selectedTodo, 1);
   res.send("Todo Successfully Deleted");
 });
 
